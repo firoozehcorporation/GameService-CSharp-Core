6552cb9b7aa0d692b3f4c51e91620f2b48d5f3d4
VIP_eR <mhzare2004@gmail.com>
2019-11-11 21:41:36 +0330

    updated gitignore FOR LOVE OF GOD

--- FiroozehGameService/Models/EventArgs/DownloadProgressArgs.cs
+++
@@ -1,9 +0,0 @@
-namespace FiroozehGameService.Models.EventArgs
-{
-    public class DownloadProgressArgs:System.EventArgs
-    {
-        public long ProgessSize;
-        public long TotalSize;
-        public byte[] data;
-    }
-}
