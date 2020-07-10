9aa12c23ed6699a5251511b8e63d3f8f48b03bd1
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-07-10 11:07:05 +0430

    Added PacketBinary Class

---
+++ FiroozehGameService/Models/Internal/PacketBinary.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+
+namespace FiroozehGameService.Models.Internal
+{
+    [Serializable]
+    internal abstract class PacketBinary
+    {
+        internal abstract byte[] Serialize();
+        internal abstract void Deserialize(byte[] buffer);
+        internal abstract int BufferSize(short lenSize);
+
+        internal string ConvertToString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data);
+        }
+
+        internal byte[] ConvertToBytes(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+    }
+}
