d5add4aa43eac24d4b6bfbe6b91e249df58bc03e
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-07-11 22:47:34 +0430

    Migrate To Binary System (Added Classes)

--- FiroozehGameService/Core/Socket/PacketHelper/PacketDeserializer.cs
+++ FiroozehGameService/Core/Socket/PacketHelper/PacketDeserializer.cs
@@ -1,23 +1,31 @@
 using System;
 using System.Linq;
 using FiroozehGameService.Models.Enums.GSLive;
+using FiroozehGameService.Models.GSLive;
+using FiroozehGameService.Models.GSLive.Command;
 using FiroozehGameService.Utils;
-using GameServiceHelper.Utils;
 
 namespace FiroozehGameService.Core.Socket.PacketHelper
 {
     internal class PacketDeserializer : IDeserializer
     {
-        public string Deserialize(byte[] buffer, int offset, int receivedBytes,string pwd,GSLiveType type)
+        public APacket Deserialize(byte[] buffer, int offset, int receivedBytes,GSLiveType type)
         {
             try
             {
                 var seg = new ArraySegment<byte>(buffer,offset,receivedBytes);
-                var deserialize = Serializer.Deserialize(seg.ToArray(), KeyTypeUtil.GetPwd(pwd,type));
-               
-                LogUtil.Log(this,"PacketDeserializer Key: " + KeyTypeUtil.GetPwd(pwd,type));
-                LogUtil.Log(this,"PacketDeserializer : " + deserialize);
-                return deserialize;
+                switch (type)
+                {
+                    case GSLiveType.Core:
+                    case GSLiveType.TurnBased:
+                        return new Packet(seg.ToArray());
+                    case GSLiveType.RealTime:
+                        return new Models.GSLive.RT.Packet(seg.ToArray());
+                    case GSLiveType.NotSet:
+                        throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                }
             }
             catch (Exception e)
             {
@@ -26,19 +34,5 @@ namespace FiroozehGameService.Core.Socket.PacketHelper
             }
            
         }
-
-        public string Deserialize(byte[] buffer,string pwd,GSLiveType type)
-        {
-            try
-            {
-                LogUtil.Log(this,"PacketDeserializer Rec :" + buffer.Length +" Bytes");
-                return Serializer.Deserialize(buffer, KeyTypeUtil.GetPwd(pwd,type));
-            }
-            catch (Exception e)
-            {
-                LogUtil.LogError(this,"PacketDeserializer Err : " + e.Message);
-                return null;
-            }
-        }        
     }
 }
