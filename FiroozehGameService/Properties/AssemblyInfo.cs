7eaa339f9a2ffa334ed18263ab503a9001bf3f5a
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2021-06-19 11:11:39 +0430

    Upgraded AssemblyInfo Version Updated GProtocol Lib Removed Unused Libs

--- FiroozehGameService/Properties/AssemblyInfo.cs
+++ FiroozehGameService/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Firoozeh Corp LTD")]
 [assembly: AssemblyProduct("FiroozehGameService")]
-[assembly: AssemblyCopyright("Copyright © 2020 Firoozeh Corp LTD")]
+[assembly: AssemblyCopyright("Copyright © 2021 Firoozeh Corp LTD")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyVersion("8.0.0")]
+[assembly: AssemblyFileVersion("8.0.0")]
